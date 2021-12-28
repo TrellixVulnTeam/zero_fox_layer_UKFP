55ac04d282d54bceb4e2e30a672c22c6d4c1c65c
glitch <saniltison@gmail.com>
2021-12-27 19:36:21 -0500

    did the following: 	- napalm related 		- create a mock napalm driver then a mock device to conduct 		...device testing 		- created mock interface data for the device 		- create a fucntion in "my_libs" to get interfaces info from the 		...mock device 		- created an endpoint to use that function 		- tested that endpoint with SAT results
    EOC

--- using_flask_NAPALM_only/libs/my_libs.py
+++ using_flask_NAPALM_only/libs/my_libs.py
@@ -1,10 +1,20 @@
 from string import Template
-
+from typing import Optional
+import napalm
 
 # create class to config hostname on cisco IOS
 ## use Template module to create a config file to change the device hostname
 ## use the config file with NAPALM to config new hostname on device
-    
+    # #--------DURING DEVELOPMENT ONLY--------#
+    # create a NAPALM driver using the mock driver
+    # instantiate from that driver and add attrbs to represent a device
+
+
+    # create a NAPALM driver using the mock driver
+
+
+
+
 def chg_dev_hostname(new_name):
     # use Template module to create a config file to change the device hostname
     t = Template('hostname $new_namePH')
@@ -15,3 +25,15 @@ def chg_dev_hostname(new_name):
         file1.write(entry)
     # entry02 = f'config file created'
     # return entry02
+    
+    
+        # instantiate from that driver and add attrbs to represent a device
+    
+
+
+def get_interfaces_info():
+    driver = napalm.get_network_driver('mock')
+    with driver('R1', 'cisco', 'cisco12345', optional_args={'path': '/home/glitch/Documents/projects/zero_fox_layer/using_flask_NAPALM_only/napalm_dev_mock_driver_related'}) as device:
+        device.open()
+        info = device.get_interfaces() # napalm call no.1
+        return info
--- using_flask_NAPALM_only/zero_fox_layer_flask.py
+++ using_flask_NAPALM_only/zero_fox_layer_flask.py
@@ -4,8 +4,9 @@
 # %%
 from flask import Flask, request
 from flask_restful import Resource, Api
+from napalm.base import get_network_driver
 from pydantic import BaseModel
-from libs.my_libs import chg_dev_hostname
+from libs.my_libs import chg_dev_hostname, get_interfaces_info
 import json
 
 app = Flask(__name__)
@@ -14,10 +15,23 @@ api = Api(app)
 # *---CISCO DEVICES ONLY---*
 
 ##### *---CISCO IOS DEVICES ONLY---*
+# -------- ALL GET ENDPOITS --------
+
 class GetDeviceConfig(Resource):
     def get(self):
         return {'message': 'place device config here'}
 
+class GetIntsinfo(Resource):
+    def get(self):
+        resp = get_interfaces_info()
+        return resp
+
+# -------- END OF ALL GET ENDPOITS --------
+
+
+
+
+# -------- ALL POST ENDPOITS --------
 class SendDeviceConfig(Resource):
     def post(self):
         form_data = request.form['new_dev_hostname']
@@ -29,10 +43,13 @@ class ConfigDevHostname(Resource):
         form_data = request.form['new_dev_hostname']
         resp_msg = chg_dev_hostname(form_data)
         return {'response message': resp_msg}
-        
+
+# -------- END OF ALL POST ENDPOITS --------
+
 
 
 api.add_resource(GetDeviceConfig, '/cisco/ios/get_device_config')
+api.add_resource(GetIntsinfo, '/cisco/ios/get_interfaces_info')
 api.add_resource(SendDeviceConfig, '/cisco/ios/send_device_config')
 api.add_resource(ConfigDevHostname, '/cisco/ios/chg_dev_hostname')
 
